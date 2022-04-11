92f998ff02f00c1a27bf8f773cc46f2a14a202de
Yash Arora <yasharora9416@gmail.com>
2022-04-11 10:59:38 +0530

    Added Basic Data Struct

--- lib/data.dart
+++ lib/data.dart
@@ -1,3 +1,82 @@
+const User currentUser = User(
+  username: 'Flutter',
+  profileImageUrl:
+      'https://yt3.ggpht.com/ytc/AKedOLQszIStjOIdSZYf4zDliwpynC69DDn0ZIJFwuP00w=-c-k-c0x00ffffff-no-rj',
+  subscribers: '100K',
+);
+final List<Video> videos = [
+  Video(
+    id: 'yll3SNXvQCw',
+    author: currentUser,
+    title: 'Flutter Engage',
+    thumbnailUrl: 'https://i.ytimg.com/vi/yll3SNXvQCw/0.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10K',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    author: currentUser,
+    id: '7_v6oMtz7tA',
+    title:
+        'MyDash: It\'s not just one big build method anymore! | Learning to Fly',
+    thumbnailUrl: 'https://i.ytimg.com/vi/7_v6oMtz7tA/0.jpg',
+    duration: '22:06',
+    timestamp: DateTime(2021, 2, 26),
+    viewCount: '8K',
+    likes: '485',
+    dislikes: '8',
+  ),
+  Video(
+    id: 'NG6pvXpnIso',
+    author: currentUser,
+    title: 'Flow (Flutter Widget of the Week)',
+    thumbnailUrl: 'https://i.ytimg.com/vi/NG6pvXpnIso/0.jpg',
+    duration: '10:53',
+    timestamp: DateTime(2020, 7, 12),
+    viewCount: '18K',
+    likes: '1k',
+    dislikes: '4',
+  ),
+];
+
+final List<Video> suggestedVideos = [
+  Video(
+    id: 'xWV71C2kp38',
+    author: currentUser,
+    title: 'How do I make my first Flutter app',
+    thumbnailUrl: 'https://i.ytimg.com/vi/xWV71C2kp38/0.jpg',
+    duration: '1:13:15',
+    timestamp: DateTime(2020, 8, 22),
+    viewCount: '32K',
+    likes: '1.9k',
+    dislikes: '7',
+  ),
+  Video(
+    id: 'QlwiL_yLh6E',
+    author: currentUser,
+    title: 'What is State?',
+    thumbnailUrl: 'https://i.ytimg.com/vi/QlwiL_yLh6E/0.jpg',
+    duration: '1:52:12',
+    timestamp: DateTime(2020, 8, 7),
+    viewCount: '190K',
+    likes: '9.3K',
+    dislikes: '45',
+  ),
+  Video(
+    id: 'rIaaH87z1-g',
+    author: currentUser,
+    title: 'BuildContext?! | Decoding Flutter',
+    thumbnailUrl: 'https://i.ytimg.com/vi/rIaaH87z1-g/0.jpg',
+    duration: '1:03:58',
+    timestamp: DateTime(2019, 10, 17),
+    viewCount: '358K',
+    likes: '20k',
+    dislikes: '85',
+  ),
+];
+
 class User {
   final String username;
   final String profileImageUrl;
@@ -13,7 +92,7 @@ class Video {
   final String id;
   final User author;
   final String title;
-  final String thumbnailurl;
+  final String thumbnailUrl;
   final String duration;
   final DateTime timestamp;
   final String viewCount;
@@ -23,7 +102,7 @@ class Video {
     required this.id,
     required this.author,
     required this.title,
-    required this.thumbnailurl,
+    required this.thumbnailUrl,
     required this.duration,
     required this.timestamp,
     required this.viewCount,
