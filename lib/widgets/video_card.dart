d8da25de1dc8f7ed856fc2b10f25723580c21452
Yash Arora <yasharora9416@gmail.com>
2022-04-12 14:29:53 +0530

    Completed the UI

--- lib/widgets/video_card.dart
+++ lib/widgets/video_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 import '../data.dart';
 
@@ -21,22 +22,76 @@ class VideoCard extends StatelessWidget {
             Positioned(
               bottom: 8,
               right: 8,
-              child: Padding(
+              child: Container(
                 padding: const EdgeInsets.all(4.0),
-                child: Container(
-                  color: Colors.black,
-                  child: Text(
-                    video.duration,
-                    // style: Theme.of(context)
-                    //     .textTheme
-                    //     .caption!
-                    //     .copyWith(color: Colors.white),
-                  ),
+                color: Colors.black,
+                child: Text(
+                  video.duration,
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption!
+                      .copyWith(color: Colors.white),
                 ),
               ),
-            )
+            ),
           ],
         ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  print('Navigate to Profile');
+                },
+                child: CircleAvatar(
+                  foregroundImage: NetworkImage(video.author.profileImageUrl),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        video.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(fontSize: 15.0),
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        '${video.author.username} • ${video.viewCount} views • ${timeago.format(video.timestamp)}',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption!
+                            .copyWith(fontSize: 15.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () => print('Options'),
+                child: const Icon(
+                  Icons.more_vert,
+                  size: 20,
+                ),
+              )
+            ],
+          ),
+        )
       ],
     );
   }
